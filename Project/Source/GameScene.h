42b12ac344d3fdc0836f1aa8c67bb9983818d74a
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-12-05 15:01:21 +0900

    gameScene作成

---
+++ Project/Source/GameScene.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <eScene/BaseScene.h>
+#include <memory>
+
+class GameScene : public BaseScene
+{
+public:
+
+    static std::unique_ptr<BaseScene> Create();
+
+     ~GameScene() override;
+
+    void Initialize() override;
+    void Update() override;
+    void Draw() override;
+
+private:
+
+#ifdef _DEBUG
+    void ImGui();
+#endif // _DEBUG
+};

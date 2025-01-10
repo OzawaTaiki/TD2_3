143266d2d788b9dba6f194b6c966e5e64de0fc4b
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-01-10 21:10:48 +0900

    Enemyクラスの作成

--- Project/Source/Objects/Entity/Enemy/Enemy.cpp
+++ Project/Source/Objects/Entity/Enemy/Enemy.cpp
@@ -1,1 +1,19 @@
 #include "Enemy.h"
+
+void Enemy::Initialize(Camera* camera)
+{
+	camera_ = camera;
+
+	oModel_ = std::make_unique<ObjectModel>();
+	oModel_->Initialize("human/walk.gltf", "Enemy");
+}
+
+void Enemy::Update()
+{
+	oModel_->Update();
+}
+
+void Enemy::Draw(const Vector4& color)
+{
+	oModel_->Draw(camera_, color);
+}
--- Project/Source/Objects/Entity/Enemy/Enemy.h
+++ Project/Source/Objects/Entity/Enemy/Enemy.h
@@ -1,5 +1,29 @@
 #pragma once
-class Enemy
+
+#include "../BaseEntity.h"
+class Enemy : public BaseEntity
 {
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(Camera* camera) override;
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update() override;
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(const Vector4& color) override;
+
+
+private:
+	
+
+
 };
 

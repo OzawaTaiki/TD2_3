a6a8f584fc42028cbfdd23065dc353a5d84a7a09
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-01-27 21:39:59 +0900

    一旦リソース追加 リザルトシーン！！！！！！！！

--- Project/Source/Scene/ResultScene.cpp
+++ Project/Source/Scene/ResultScene.cpp
@@ -5,6 +5,9 @@
 #include "../System_TD/ScoreManager/ScoreManager.h"
 #include "../System_TD/ComboManager/ComboManager.h"
 #include "../System_TD/CountManager/CountManager.h"
+#include <ResourceManagement/TextureManager/TextureManager.h>
+
+const std::string defaulFilPath = "Resources./Textures./";
 
 std::unique_ptr<BaseScene> ResultScene::Create()
 {
@@ -17,6 +20,27 @@ ResultScene::~ResultScene()
 
 void ResultScene::Initialize()
 {
+	uint32_t th[ms_] = { TextureManager::GetInstance()->Load("backGround.png", defaulFilPath),
+						 TextureManager::GetInstance()->Load("frame.png", defaulFilPath),
+						 TextureManager::GetInstance()->Load("result.png", defaulFilPath) };
+	
+	resultSprite_[0] = std::make_unique<Sprite>();
+	resultSprite_[0].reset(Sprite::Create(th[0]));
+	resultSprite_[0]->Initialize();
+	resultSprite_[0]->translate_ = {640,360};
+
+	resultSprite_[1] = std::make_unique<Sprite>();
+	resultSprite_[1].reset(Sprite::Create(th[1]));
+	resultSprite_[1]->Initialize();
+	resultSprite_[1]->translate_ = { 640,360 };
+
+	resultSprite_[2] = std::make_unique<Sprite>();
+	resultSprite_[2].reset(Sprite::Create(th[2]));
+	resultSprite_[2]->Initialize();
+	resultSprite_[2]->SetAnchor({ 0.5f,0.5f });
+	resultSprite_[2]->translate_ = { 640,360 };
+	
+
 }
 
 void ResultScene::Update()
@@ -30,10 +54,17 @@ void ResultScene::Update()
 	ScoreManager::GetInstance()->Update();
 	ComboManager::GetInstance()->Update();
 	CountManager::GetInstance()->ImGui();
+
+	
 }
 
 void ResultScene::Draw()
 {
+	Sprite::PreDraw();
+	for (uint32_t i = 0; i < ms_; ++i)
+	{
+		resultSprite_[i]->Draw();
+	}
 }
 #ifdef _DEBUG
 void ResultScene::ImGui()
